31c0fd69614046a7de6a6d0e307fa4a62f668251
Sara Gomes <sarah.gomes80@hotmail.com>
2024-03-25 12:25:24 -0300

    0202 Link

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -9,6 +9,9 @@ export default function SobrePage() {
   return (
     <main>
       <h2>Sobre</h2>
+      <h2 style={{ margin: '1600px 0' }} id="empresa">
+        A Empresa
+      </h2>
     </main>
   );
 }
